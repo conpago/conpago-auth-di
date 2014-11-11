d89509d8bf3514fd69f1b7029d7f80be47d31798
Bartosz Golek <bartosz.golek@gmail.com>
2014-11-11 14:04:58 +0100

    init Auth.DI

---
+++ src/Saigon/Conpago/Auth/AuthModule.php
@@ -0,0 +1,26 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Bartosz Gołek
+	 * Date: 2014-06-30
+	 * Time: 23:04
+	 */
+
+	namespace Saigon\Conpago\Auth;
+
+	use Saigon\Conpago\DI\IContainerBuilder;
+	use Saigon\Conpago\IModule;
+
+	class AuthModule implements IModule
+	{
+		public function build(IContainerBuilder $builder)
+		{
+			$builder
+				->registerType('Saigon\Conpago\Auth\Session')
+				->asA('Saigon\Conpago\Auth\Contract\ISession');
+
+			$builder
+				->registerType('Saigon\Conpago\Auth\SessionManager')
+				->asA('Saigon\Conpago\Auth\Contract\ISessionManager');
+		}
+	}
